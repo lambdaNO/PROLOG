6f30163d599b8a6f9559cbd8c430e181828e4190
latifmehdi <mehlatif44@gmail.com>
2017-02-04 11:43:02 +0100

    Correction de l'erreur dans le TP1

--- TP1/myPrologCode.pl
+++ TP1/myPrologCode.pl
@@ -4,7 +4,7 @@
 %% I. UNIFICATION ('_' : variable anonyme):
 
 e0(1,[X,Y]) :- f(X,a,Y) = f(b,Y,_).
-e0un :- f(X,a,Y) = f(b,Y,c).
+e0un(1,[X,Y]) :- f(X,a,Y) = f(b,Y,c).
 /*Va retourner false  car on ne peut ecraser la valeur d'une variable par une autre Y=a,Y=c - C'est MEGA FALSE (et normal)*/
 e0deux(1,[X,Y,Z]) :- f(X,a,Z) = f(b,Y,c).
 
