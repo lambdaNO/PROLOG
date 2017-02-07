12579afb1dd8cd917580b0569fb7fa5db4a4aa95
Mehdi LATIF <E131729J@I123V7pc02.ensinfo.sciences.univ-nantes.prive>
2017-02-07 09:16:30 +0100

    Session de TP 3 + détail dans le cahier

--- TP1/myPrologCode.pl
+++ TP1/myPrologCode.pl
@@ -4,7 +4,15 @@
 %% I. UNIFICATION ('_' : variable anonyme):
 
 e0(1,[X,Y]) :- f(X,a,Y) = f(b,Y,_).
-e0un(1,[X,Y]) :- f(X,a,Y) = f(b,Y,c).
+e0a(1,[X,Y]) :- f(X,a,Y) = f(b,Y,c).
 /*Va retourner false  car on ne peut ecraser la valeur d'une variable par une autre Y=a,Y=c - C'est MEGA FALSE (et normal)*/
-e0deux(1,[X,Y,Z]) :- f(X,a,Z) = f(b,Y,c).
+e0b(1,[X,Y,Z]) :- f(X,a,Z) = f(b,Y,c).
 
+e0(2,[X,Y]) :- f(X,_,h(Y,Y,c)) = f(Z,0,h(k(X),V,X)).
+e0a(2,[X,Y,A]) :- f(X,A,h(Y,Y,c)) = f(Z,0,h(k(X),V,X)).
+
+e0(3,[U]) :- f(X,h(X,X),k(Y,Y)) = f(g(0),Y,U).
+
+
+
+/*Pour la fonction pro ... faire l'arbre pour bien comprendre ... :) */
